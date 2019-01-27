4aea4fd0b764ffd54e269b37c792cf1ad4fb2975
Glukoosi <aleksi@seppanen.me>
2019-01-27 15:38:12 +0200

    Cleanup

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from app import db
 class Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     string = db.Column(db.String(64))
-    string3 = db.Column(db.String(64))
     boolean = db.Column(db.Boolean())
     radio = db.Column(db.String(64))
     text = db.Column(db.String(500))
